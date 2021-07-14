5e045382ae14223d4450e7fc07fddb6fd777db38
season91 <choayoung91@naver.com>
2021-07-14 20:18:01 +0900

    command map

--- 02study/src/main/java/com/wisenut/study/common/CommandMap.java
+++ 02study/src/main/java/com/wisenut/study/common/CommandMap.java
@@ -1,5 +1,14 @@
 package com.wisenut.study.common;
 
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class CommandMap {
+	
+	//vo 대신 커맨드 맵 사용 
+	public void test () {
+		Map<String, Object> commadMap = new HashMap<>();
+	}
 
 }
